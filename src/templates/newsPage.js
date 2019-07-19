78ef247c9da5c7c382cbe15ca07dee297545e870
Tim Ermilov <yamalight@gmail.com>
2019-07-19 14:16:13 +0200

    Add thumbnail display to news article template

--- src/templates/newsPage.js
+++ src/templates/newsPage.js
@@ -1,6 +1,7 @@
-import React from 'react';
 import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
+import React from 'react';
+import Image from '../components/image';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
@@ -12,9 +13,18 @@ export default function Template({
   return (
     <Layout>
       <SEO title={frontmatter.title} />
-      <h1>{frontmatter.title}</h1>
-      <div>Published on: {frontmatter.date}</div>
-      <MDXRenderer>{body}</MDXRenderer>
+      <div className="content">
+        <h1>{frontmatter.title}</h1>
+        <div>Published on: {frontmatter.date}</div>
+        {frontmatter.thumbnail ? (
+          <div className="news-thumbnail">
+            <Image filename={frontmatter.thumbnail} />
+          </div>
+        ) : (
+          ''
+        )}
+        <MDXRenderer>{body}</MDXRenderer>
+      </div>
     </Layout>
   );
 }
@@ -25,6 +35,7 @@ export const pageQuery = graphql`
       frontmatter {
         title
         date
+        thumbnail
       }
       body
     }
