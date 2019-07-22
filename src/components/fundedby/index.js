5209249550b800f795f066633c14374e31d2ad8d
Tim Ermilov <yamalight@gmail.com>
2019-07-22 13:41:22 +0200

    Add funded-by images to project

--- src/components/fundedby/index.js
+++ src/components/fundedby/index.js
@@ -1,44 +1,45 @@
 import React from 'react';
+import Image from '../image';
 
 const fundedBy = [
   {
     url: 'http://www.bmbf.de/',
-    image: 'https://dice-research.org/fileadmin/Front_Logos/logo-bmbf.png',
+    image: 'logo-bmbf.png',
     text: 'BMBF',
   },
   {
     url: 'http://www.bmwi.de/',
-    image: 'https://dice-research.org/fileadmin/Front_Logos/logo-bmwi.png',
+    image: 'logo-bmwi.png',
     text: 'BMWI',
   },
   {
     url: 'http://www.bmvi.de/',
-    image: 'https://dice-research.org/fileadmin/Front_Logos/logo-bmVI.png',
+    image: 'logo-bmVI.png',
     text: 'BMVI',
   },
   {
     url: 'http://www.eurostars.dlr.de/',
-    image: 'https://dice-research.org/fileadmin/Front_Logos/logo-eurostars.png',
+    image: 'logo-eurostars.png',
     text: 'EuroStars',
   },
   {
     url: 'http://cordis.europa.eu/',
-    image: 'https://dice-research.org/fileadmin/Front_Logos/logo-fp7.png',
+    image: 'logo-fp7.png',
     text: 'FP7',
   },
   {
     url: 'http://www.daad.de/',
-    image: 'https://dice-research.org/fileadmin/Front_Logos/logo-daad.png',
+    image: 'logo-daad.png',
     text: 'DAAD',
   },
   {
     url: 'http://ec.europa.eu/',
-    image: 'https://dice-research.org/fileadmin/Front_Logos/horizon2020.png',
+    image: 'horizon2020.png',
     text: 'Horizon2020',
   },
   {
     url: 'http://www.dfg.de/',
-    image: 'https://dice-research.org/fileadmin/Front_Logos/logo-dfg.png',
+    image: 'logo-dfg.png',
     text: 'DFG',
   },
 ];
@@ -55,7 +56,7 @@ const FundedBy = () => (
   >
     {fundedBy.map(org => (
       <a key={org.url} href={org.url}>
-        <img src={org.image} alt={org.text} />
+        <Image filename={org.image} alt={org.text} style={{ width: 80 }} />
       </a>
     ))}
   </div>
