e1ef18a9733e997d5f90160f8426f3387ada0a6b
Tim Ermilov <yamalight@gmail.com>
2019-08-01 12:40:04 +0200

    Add basic project search

--- scripts/src/pages/projects.js
+++ scripts/src/pages/projects.js
@@ -1,6 +1,5 @@
-import { graphql } from 'gatsby';
 import _ from 'lodash';
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import Layout from '../components/layout';
 import Project from '../components/project';
 import SEO from '../components/seo';
@@ -10,15 +9,27 @@ export default function Projects({
     allRdf: { edges },
   },
 }) {
-  const projectsByType = _.groupBy(
-    edges,
-    p => p.node.data.rdf_type[0].data.name
+  const [search, setSearch] = useState('');
+
+  const projectsByType = useMemo(
+    () => _.groupBy(edges, p => p.node.data.rdf_type[0].data.name),
+    [edges]
   );
+
+  const handleSearch = e => setSearch(e.target.value);
 
   return (
     <Layout>
       <SEO title="Projects" />
       <div className="content">
+        <div style={{ padding: 20 }}>
+          <input
+            className="input is-rounded"
+            type="text"
+            placeholder="Search for project.."
+            onChange={handleSearch}
+          />
+        </div>
         {Object.keys(projectsByType).map(type => (
           <div
             key={type}
@@ -27,9 +38,15 @@ export default function Projects({
           >
             <h2 style={{ marginBottom: '1em' }}>{type}</h2>
             <div className="tile is-ancestor">
-              {projectsByType[type].map(project => (
-                <Project key={project.path} project={project.node} />
-              ))}
+              {projectsByType[type]
+                .filter(project =>
+                  project.node.data.name
+                    .toLowerCase()
+                    .includes(search.toLowerCase())
+                )
+                .map(project => (
+                  <Project key={project.node.path} project={project.node} />
+                ))}
             </div>
           </div>
         ))}
