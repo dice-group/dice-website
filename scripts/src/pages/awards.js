3f008112111ee02e45fbe6030aacde7e2a6e06e4
Tim Ermilov <yamalight@gmail.com>
2019-08-01 13:28:39 +0200

    Better awards rendering

--- scripts/src/pages/awards.js
+++ scripts/src/pages/awards.js
@@ -1,5 +1,6 @@
-import { graphql, Link } from 'gatsby';
+import { graphql } from 'gatsby';
 import React from 'react';
+import Award from '../components/award';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
@@ -12,28 +13,8 @@ export default function Awards({
     <Layout>
       <SEO title="Projects" />
       <div className="content tile is-ancestor is-vertical">
-        {edges.map(({ node: { id, data } }) => (
-          <div
-            key={id}
-            className="tile is-vertical"
-            style={{ marginBottom: '2em' }}
-          >
-            <h5 className="title is-4">
-              <a href={data.url}>{data.name}</a>
-              <span className="tag" style={{ marginLeft: 10 }}>
-                {data.year}
-              </span>
-            </h5>
-            <p className="subtitle">{data.content}</p>
-            <div className="tile">
-              <div style={{ marginRight: 10 }}>Awarded to:</div>
-              {data.awardee.map(person => (
-                <span key={person.path}>
-                  <Link to={person.path}>{person.data.name}</Link>
-                </span>
-              ))}
-            </div>
-          </div>
+        {edges.map(({ node }) => (
+          <Award key={node.id} node={node} />
         ))}
       </div>
     </Layout>
