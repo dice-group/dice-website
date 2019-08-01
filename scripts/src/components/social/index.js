3f4d16f933799cd900ff4ab8116967dd4a98cc8a
Tim Ermilov <yamalight@gmail.com>
2019-08-01 11:55:02 +0200

    Add links to social media

---
+++ scripts/src/components/social/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { FaFacebookF, FaGithub, FaTwitter, FaYoutube } from 'react-icons/fa';
+
+const links = [
+  {
+    url: 'https://www.facebook.com/DiceUPB',
+    text: 'Facebook',
+    icon: FaFacebookF,
+  },
+  { url: 'https://twitter.com/DiceResearch', text: 'Twitter', icon: FaTwitter },
+  { url: 'https://github.com/dice-group', text: 'GitHub', icon: FaGithub },
+  {
+    url: 'https://www.youtube.com/channel/UCDshdIaiXqwZsQ4Gl_70OMA',
+    text: 'YouTube',
+    icon: FaYoutube,
+  },
+];
+
+const Social = () => (
+  <div
+    className="column is-flex"
+    style={{
+      justifyContent: 'space-evenly',
+      alignItems: 'center',
+      minWidth: 150,
+    }}
+  >
+    {links.map(l => (
+      <a key={l.url} href={l.url} title={l.text}>
+        <span class="icon">
+          <l.icon />
+        </span>
+      </a>
+    ))}
+  </div>
+);
+
+export default Social;
