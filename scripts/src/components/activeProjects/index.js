0e4a61ae846268cb89128c456417fdaaae44467e
Tim Ermilov <yamalight@gmail.com>
2019-08-19 11:10:16 +0200

    Apply new design to projects page

--- scripts/src/components/activeProjects/index.js
+++ scripts/src/components/activeProjects/index.js
@@ -1,6 +1,6 @@
-import { graphql, Link, useStaticQuery } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
-import Image from '../image';
+import Project from '../project';
 
 const newsQuery = graphql`
   {
@@ -56,19 +56,8 @@ const ActiveProjects = () => {
   return (
     <div className="columns">
       {edges.map(({ node }) => (
-        <div className="column is-one-third active-project" key={node.path}>
-          <div className="project-image">
-            <Image
-              filename={node.data.logo}
-              alt={`${node.data.name} logo`}
-              style={{ width: 200 }}
-            />
-          </div>
-          <h2 className="title">{node.data.name}</h2>
-          <div className="separator" />
-          <Link to={node.path} className="link has-text-grey">
-            Learn more →
-          </Link>
+        <div className="column is-one-third" key={node.path}>
+          <Project project={node} />
         </div>
       ))}
     </div>
--- scripts/src/components/project/index.js
+++ scripts/src/components/project/index.js
@@ -3,31 +3,19 @@ import React from 'react';
 import Image from '../image';
 
 const Project = ({ project, renderType = true }) => (
-  <div key={project.path} className="card" style={{ margin: '1em' }}>
-    <div className="card-content">
-      <div className="media">
-        <div className="media-left">
-          <figure className="image gatsby-image is-64x64">
-            <Image
-              filename={project.data.logo}
-              alt={`${project.data.name} logo`}
-            />
-          </figure>
-        </div>
-        <div className="media-content">
-          <p className="title is-4">
-            <Link to={project.path}>{project.data.name}</Link>
-          </p>
-          {renderType && (
-            <p className="subtitle is-6">
-              {project.data.rdf_type[0].data.name}
-            </p>
-          )}
-        </div>
-      </div>
-
-      <div className="content">{project.data.tagline}</div>
+  <div className="active-project">
+    <div className="project-image">
+      <Image
+        filename={project.data.logo}
+        alt={`${project.data.name} logo`}
+        style={{ width: 200 }}
+      />
     </div>
+    <h2 className="title">{project.data.name}</h2>
+    <div className="separator" />
+    <Link to={project.path} className="link">
+      Learn more
+    </Link>
   </div>
 );
 
