0a07b2ce108af344b16df8d21d79789bd4571f2f
Tim Ermilov <yamalight@gmail.com>
2019-08-30 10:04:57 +0200

    Update dependencies and fix linting issues

--- scripts/src/components/sidemenu/index.js
+++ scripts/src/components/sidemenu/index.js
@@ -1,3 +1,4 @@
+/* global IntersectionObserver */
 import React, { useEffect, useState } from 'react';
 import Item from './item';
 
--- scripts/src/pages/projects.js
+++ scripts/src/pages/projects.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
-import React, { useMemo, useState } from 'react';
+import { graphql } from 'gatsby';
+import React, { useMemo } from 'react';
 import Layout from '../components/layout';
 import Project from '../components/project';
 import SEO from '../components/seo';
@@ -9,63 +10,31 @@ export default function Projects({
     allRdf: { edges },
   },
 }) {
-  const [search, setSearch] = useState('');
-
   const projectsByType = useMemo(
     () => _.groupBy(edges, p => p.node.data.rdf_type[0].data.name),
     [edges]
   );
 
-  const handleSearch = e => setSearch(e.target.value);
-
   return (
     <Layout>
       <SEO title="Projects" />
       <div className="content">
-        {/*<div style={{ padding: '0 20px 20px 20px' }}>
-          <input
-            className="input is-rounded"
-            type="text"
-            placeholder="Search for project.."
-            onChange={handleSearch}
-          />
-        </div>*/}
-        {Object.keys(projectsByType)
-          .filter(type => {
-            return (
-              projectsByType[type].filter(project =>
-                project.node.data.name
-                  .toLowerCase()
-                  .includes(search.toLowerCase())
-              ).length > 0
-            );
-          })
-          .map(type => (
-            <div
-              key={type}
-              className="tile is-vertical"
-              style={{ marginBottom: '3em' }}
-            >
-              <h1 style={{ marginBottom: '1em' }}>{type}</h1>
-              <div className="columns is-multiline is-5 is-variable">
-                {projectsByType[type]
-                  .filter(project =>
-                    project.node.data.name
-                      .toLowerCase()
-                      .includes(search.toLowerCase())
-                  )
-                  .map(({ node }) => (
-                    <div className="column is-one-third" key={node.path}>
-                      <Project
-                        key={node.path}
-                        project={node}
-                        renderType={false}
-                      />
-                    </div>
-                  ))}
-              </div>
+        {Object.keys(projectsByType).map(type => (
+          <div
+            key={type}
+            className="tile is-vertical"
+            style={{ marginBottom: '3em' }}
+          >
+            <h1 style={{ marginBottom: '1em' }}>{type}</h1>
+            <div className="columns is-multiline is-5 is-variable">
+              {projectsByType[type].map(({ node }) => (
+                <div className="column is-one-third" key={node.path}>
+                  <Project key={node.path} project={node} renderType={false} />
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
+        ))}
       </div>
     </Layout>
   );
