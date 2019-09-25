390fb80254fe747955a2fc2dcf198bfe23244c2d
Tim Ermilov <yamalight@gmail.com>
2019-09-25 10:54:31 +0200

    Open partners in new tab

--- scripts/src/components/partner/index.js
+++ scripts/src/components/partner/index.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Image from '../image';
 
-const Partner = ({ partner, renderType = true }) => (
-  <div
-    className="partner"
-    onClick={() => {
-      window.location.href = partner.data.url;
-    }}
-  >
-    <div className="project-image">
-      <Image
-        filename={partner.data.logo}
-        alt={`${partner.data.name} logo`}
-        style={{ width: 200 }}
-      />
+const Partner = ({ partner, renderType = true }) => {
+  const url = useRef();
+  return (
+    <div className="partner" onClick={() => url.current && url.current.click()}>
+      <div className="project-image">
+        <Image
+          filename={partner.data.logo}
+          alt={`${partner.data.name} logo`}
+          style={{ width: 200 }}
+        />
+      </div>
+      <a
+        className="name"
+        href={partner.data.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        ref={url}
+      >
+        {partner.data.name}
+      </a>
     </div>
-    <a className="name" href={partner.data.url}>
-      {partner.data.name}
-    </a>
-  </div>
-);
+  );
+};
 
 export default Partner;
