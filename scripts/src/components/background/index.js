2fbda1463cc74b045897132791bde359b844b15c
Tim Ermilov <yamalight@gmail.com>
2019-09-26 15:50:46 +0200

    Fix svg header spacing

--- scripts/src/components/background/index.js
+++ scripts/src/components/background/index.js
@@ -37,7 +37,7 @@ export default function useBackground({ width, height } = {}) {
     const html = svg.outerHTML.replace(
       '<svg width',
       '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"' +
-        'x="0px" y="0px" xml:space="preserve" width'
+        ' x="0px" y="0px" xml:space="preserve" width'
     );
     setSvgHtml(html);
   }, [width, height]);
