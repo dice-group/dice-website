bab1e07ac10218bff69fa5fc5ee2c7e5b8812f8e
pskLana <48616148+pskLana@users.noreply.github.com>
2021-11-25 20:30:27 +0100

    Changed template for funded projects #345

--- scripts/src/components/fundedby/index.js
+++ scripts/src/components/fundedby/index.js
@@ -4,56 +4,91 @@ import Image from '../image';
 const fundedBy = [
   {
     url: 'http://www.bmbf.de/',
-    image: 'logo-bmbf.png',
-    text: 'BMBF',
+    image: ['logo-bmbf.png'],
+    text: ['BMBF'],
   },
   {
     url: 'http://www.bmwi.de/',
-    image: 'logo-bmwi.png',
-    text: 'BMWI',
+    image: ['logo-bmwi.png'],
+    text: ['BMWI'],
   },
   {
     url: 'http://www.bmvi.de/',
-    image: 'logo-bmVI.png',
-    text: 'BMVI',
+    image: ['logo-bmVI.png'],
+    text: ['BMVI'],
   },
   {
     url: 'http://www.eurostars.dlr.de/',
-    image: 'logo-eurostars.png',
-    text: 'EuroStars',
+    image: ['logo-eurostars.png'],
+    text: ['EuroStars'],
   },
   {
     url: 'http://cordis.europa.eu/',
-    image: 'logo-fp7.png',
-    text: 'FP7',
+    image: ['logo-fp7.png'],
+    text: ['FP7'],
   },
   {
     url: 'http://www.daad.de/',
-    image: 'logo-daad.png',
-    text: 'DAAD',
+    image: ['logo-daad.png'],
+    text: ['DAAD'],
   },
   {
     url: 'http://ec.europa.eu/',
-    image: 'horizon2020.png',
-    text: 'Horizon2020',
+    image: ['horizon2020.png'],
+    text: ['Horizon2020'],
   },
   {
     url: 'http://www.dfg.de/',
-    image: 'logo-dfg.png',
-    text: 'DFG',
+    image: ['logo-dfg.png'],
+    text: ['DFG'],
   },
 ];
 
-const FundedBy = () => (
-  <div className="columns">
-    {fundedBy.map(org => (
-      <div key={org.url} className="column funded-by-item">
-        <a href={org.url} target="_blank" rel="noopener noreferrer">
-          <Image filename={org.image} alt={org.text} style={{ width: 100 }} />
-        </a>
+const getProgram = fundingProgram => {
+  const fundedPrograms = fundedBy.filter(fp =>
+    // fundingProgram.toLowerCase().includes(fp.text.toLowerCase())
+    fp.text.some(t => fundingProgram.toLowerCase().includes(t.toLowerCase()))
+  );
+  return fundedPrograms || null;
+};
+
+const FundedBy = ({ fundingProgram }) =>
+  fundingProgram ? (
+    <div>
+      <span>{fundingProgram}</span>
+      <div>
+        {getProgram(fundingProgram).map(fp =>
+          fp.image.map((image, index) => (
+            <span
+              style={{ display: 'inline-block', verticalAlign: 'middle' }}
+              key={fp.url + index}
+            >
+              <a href={fp.url}>
+                <Image
+                  filename={image}
+                  style={{ width: 100 }}
+                  key={image + index}
+                />
+              </a>
+            </span>
+          ))
+        )}
       </div>
-    ))}
-  </div>
-);
+    </div>
+  ) : (
+    <div className="columns">
+      {fundedBy.map(org => (
+        <div key={org.url} className="column funded-by-item">
+          <a href={org.url} target="_blank" rel="noopener noreferrer">
+            <Image
+              filename={org.image[0]}
+              alt={org.text[0]}
+              style={{ width: 100 }}
+            />
+          </a>
+        </div>
+      ))}
+    </div>
+  );
 
 export default FundedBy;
